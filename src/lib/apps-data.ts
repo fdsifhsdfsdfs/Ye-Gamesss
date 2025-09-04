9aa8d2bd260e1aeec99c787a0180d4ec959fe327
Aviation Airlines <airlinesaviation15@gmail.com>
2025-09-04 12:55:29 +0000

    every single game crashes when u open them

--- src/lib/apps-data.ts
+++ src/lib/apps-data.ts
@@ -7,7 +7,7 @@ export const apps: App[] = [
     title: 'Sudo Flix',
     description: 'Watch your favorite movies and TV shows with a seamless streaming experience.',
     imageUrl: 'https://avatars.githubusercontent.com/u/191057509?s=200&v=4',
-    appUrl: 'https://embeddr.rhw.one/embed#https://pstream.mov',
+    appUrl: 'https://pstream.mov',
     tags: ['streaming', 'movies', 'tv shows', 'entertainment'],
     featured: true,
   },
@@ -16,7 +16,7 @@ export const apps: App[] = [
     title: 'Youtube',
     description: 'Watch, stream, and discover what the world is watching.',
     imageUrl: 'https://www.inet-web.com/Content/files/GamingChannelTips.jpg',
-    appUrl: 'https://embeddr.rhw.one/embed#https://Youtube.com',
+    appUrl: 'https://youtube.com',
     tags: ['video', 'streaming', 'entertainment'],
     featured: false,
   }
