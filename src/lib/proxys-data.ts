1142acf4f8243ee9154b67d7863630dcfe4d51b5
Aviation Airlines <airlinesaviation15@gmail.com>
2025-09-17 04:49:42 +0000

    add a proxy called Void with this image "https://iili.io/KRjiqEQ.jpg" an

--- src/lib/proxys-data.ts
+++ src/lib/proxys-data.ts
@@ -10,5 +10,14 @@ export const proxys: Proxy[] = [
     proxyUrl: 'https://space.nearpod.lol.cdn.cloudflare.net/',
     tags: ['web'],
     featured: false,
+  },
+  {
+    id: '2',
+    title: 'Void',
+    description: 'A proxy to browse the web freely.',
+    imageUrl: 'https://iili.io/KRjiqEQ.jpg',
+    proxyUrl: 'https://www.ohiohoihohioh.voidtickets.xyz/p.html',
+    tags: ['web'],
+    featured: false,
   }
 ];
