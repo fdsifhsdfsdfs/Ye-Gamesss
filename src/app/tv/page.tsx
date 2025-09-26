06720e2d01146fc41cc124df91d2338d61e4f907
Aviation Airlines <airlinesaviation15@gmail.com>
2025-09-26 01:03:12 +0000

    add the tv page back and this the embed "https://education.hornburst.com

---
+++ src/app/tv/page.tsx
@@ -0,0 +1,12 @@
+export default function TVPage() {
+  return (
+    <div className="absolute inset-0 top-14">
+      <iframe
+        src="https://education.hornburst.com/uv/service/hvtrs8%2F-tpunycwoavh%2Cwgb%2Carp-svrgaoilg%2Chvmn"
+        className="w-full h-full border-0"
+        title="TV/Movies Page Embed"
+        allow="fullscreen"
+      />
+    </div>
+  );
+}
