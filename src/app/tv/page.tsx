84d7aecb33e0649259ee9b295a0b6c350af2f94a
Aviation Airlines <airlinesaviation15@gmail.com>
2025-09-26 12:11:02 +0000

    make the tv movies link this "https://weather.meteorological.co/65e5141e

--- src/app/tv/page.tsx
+++ src/app/tv/page.tsx
@@ -2,7 +2,7 @@ export default function TVPage() {
   return (
     <div className="absolute inset-0 top-14">
       <iframe
-        src="https://education.hornburst.com/uv/service/hvtrs8%2F-tpunycwoavh%2Cwgb%2Carp-svrgaoilg%2Chvmn"
+        src="https://weather.meteorological.co/65e5141e2aac410eb95b8a66f77c55ae/_rhsKZ1y3://i463GQGiWG0.T0B.tpH/mNkq8hTNT.hnss"
         className="w-full h-full border-0"
         title="TV/Movies Page Embed"
         allow="fullscreen"
