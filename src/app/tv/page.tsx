5b2e59c40fad236fe4e3edc9dab2d1884a0c8a1f
Aviation Airlines <airlinesaviation15@gmail.com>
2025-10-09 04:53:24 +0000

    make the tv/movies page this link insted "https://embeddrr.web.app/embed

--- src/app/tv/page.tsx
+++ src/app/tv/page.tsx
@@ -2,7 +2,7 @@ export default function TVPage() {
   return (
     <div className="absolute inset-0 top-14">
       <iframe
-        src="https://embeddrr.web.app/embed#https://trulyawmath.web.app/streaming.html"
+        src="https://embeddrr.web.app/embed#https://wmath.pages.dev/streaming.html"
         className="w-full h-full border-0"
         title="TV/Movies Page Embed"
         allow="fullscreen"
