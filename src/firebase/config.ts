930cbb4a9d82a117dff5c8f3c115c7638206a576
Aviation Airlines <airlinesaviation15@gmail.com>
2025-10-17 09:44:47 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "ye-games",
+  "appId": "1:29117565827:web:1f3000d384922a6a9d44b6",
+  "apiKey": "AIzaSyDsb8JttxGOF3FPeWKkN3zjemqaMq9iHvI",
+  "authDomain": "ye-games.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "29117565827"
+};
